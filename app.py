d2c3c7fb1e610d8eb7b39398fbaa6dfe0899585e
siddhi1602 <siddhisave16@gmail.com>
2023-06-25 09:33:28 +0530

    frontend update

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, Flask!'
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/showcards')
+def cards():
+    return render_template("Hello World")
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/recommend', methods=['GET', 'POST'])
+def recommend():
+    if request.method == 'POST':
+        form_data = request.form
+    return render_template("signup.html")
+
+@app.route('/', methods=['GET', 'POST'])
+def form_submission():
+    if request.method == 'POST':
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        emailaddress = request.form.get('emailaddress')
+        number = request.form.get('number')
+        dob = request.form.get('dob')
+        location = request.form.get('location')
+        skill = request.form.get('skill')
+        domain = request.form.get('domain')
+        exp = request.form.get('exp')
+        qua = request.form.get('qua')
+
+        return 'Form submitted successfully!'
+    # Display the form
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run()
